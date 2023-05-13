64563dfd69703af43e6816624fe404d3fbf3c29e
bisma <bisma@bismarckjrpc>
2023-05-12 21:51:05 -0300

    criação do projeto

---
+++ primeiro.projeto.java/src/cursojava/executável/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executável;
+
+public class PrimeiraClasseJava {
+
+	/*main é um metodo alto executavel em java*/
+	public static void main(String[] args) {
+
+		System.out.println("meu primeiro programa  em java");
+		System.out.println("java é orientado a objeto");
+		
+	}
+}
