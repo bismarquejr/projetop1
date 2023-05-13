1dbd1f804790de29ae7335aff84748cf6f16ecd4
bisma <bisma@bismarckjrpc>
2023-05-13 19:09:41 -0300

    adicionando o campo de idade na classe pessoa

--- primeiro.projeto.java/src/curso/java/alex/pessoa.java
+++ primeiro.projeto.java/src/curso/java/alex/pessoa.java
@@ -1,5 +1,26 @@
 package curso.java.alex;
 
 public class pessoa {
+private String name;
+
+private String sobrenome;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getSobrenome() {
+	return sobrenome;
+}
+
+public void setSobrenome(String sobrenome) {
+	this.sobrenome = sobrenome;
+}
+
+
 
 }
